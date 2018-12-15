670143ada7fb0363b610f694432d3dc6c893efea
nordineazza <nazza0609@gmail.com>
2018-12-15 13:27:12 +0100

    Contact v3

---
+++ contactv3/src/main/java/com/example/contact/config/LangConfiguration.java
@@ -0,0 +1,52 @@
+package com.example.contact.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+import java.util.Locale;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class LangConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setDefaultEncoding("UTF-8");
+        source.setCacheSeconds(-1);
+        source.setFallbackToSystemLocale(false);
+        source.setUseCodeAsDefaultMessage(true);
+        source.setBasenames("classpath:message", "classpath:langue");
+        return source;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
---
+++ contactv3/src/main/java/com/example/contact/web/ContactController.java
@@ -0,0 +1,70 @@
+package com.example.contact.web;
+
+import com.example.contact.domain.Contact;
+import com.example.contact.service.ContactService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/ContactV2")
+public class ContactController {
+    Logger logger = LoggerFactory.getLogger(ContactController.class);
+
+    @Autowired
+    private ContactService contactService;
+
+    @GetMapping
+    public ModelAndView get() {
+        List<Contact> listContact = contactService.getAll();
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("contacts", listContact);
+        logger.info(listContact.size() + " contacts sont récupérés");
+        return mv;
+    }
+
+    @PostMapping("/Add")
+    public ModelAndView addContact(@ModelAttribute("contact") Contact contact){
+        ModelAndView mv = new ModelAndView("redirect:/ContactV2");
+        try {
+            contactService.addContact(contact);
+            logger.info("Le contact a bien été ajouté.", contact);
+        } catch(ConstraintViolationException e) {
+            logger.error("Violation de contraintes");
+
+            mv.addObject("error", "Erreur internationale");
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
+        return mv;
+    }
+
+    @GetMapping("/Delete")
+    public String removeContact(@RequestParam(name="id")String id){
+        Long idContact = Long.parseLong(id);
+        contactService.deleteContact(idContact);
+        return "redirect:/ContactV2";
+    }
+
+    @GetMapping("/Update")
+    public ModelAndView updateContact(@RequestParam(name="id")String id){
+        Long idContact = Long.parseLong(id);
+        Contact c = contactService.getContact(idContact);
+        ModelAndView mv = new ModelAndView("update");
+        mv.addObject("contact", c);
+        return mv;
+    }
+
+    @PostMapping("/Update")
+    public String updateContact(@ModelAttribute("contact") Contact contact) {
+        contactService.updateContact(contact);
+        return "redirect:/ContactV2";
+    }
+
+}
